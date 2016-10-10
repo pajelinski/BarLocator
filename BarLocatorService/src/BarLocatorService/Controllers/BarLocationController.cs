0d446b78e6b63ab98da186b2530f49e70e49d24f
Pawel Jelinski <Pawel.Jelinski@powel.com>
2016-10-10 10:03:50 +0200

    Added BarLocatorService(C# version)

---
+++ BarLocatorService/src/BarLocatorService/Controllers/BarLocationController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BarLocatorService.Controllers
+{
+    [Route("api/")]
+    public class BarLocationController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
