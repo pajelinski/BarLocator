0fd3c67a4580c6c1f7dfa4128970b9c087834f7b
Pawel Jelinski <Pawel.Jelinski@powel.com>
2016-10-11 14:36:23 +0200

    Implemented BarLocationControler

--- BarLocatorService/src/BarLocatorService/Controllers/BarLocationController.cs
+++ BarLocatorService/src/BarLocatorService/Controllers/BarLocationController.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System.Threading.Tasks;
+using BarLocatorService.Domain;
+using BarLocatorService.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BarLocatorService.Controllers
@@ -6,10 +8,24 @@ namespace BarLocatorService.Controllers
     [Route("api/")]
     public class BarLocationController : Controller
     {
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private const string BarType = "bar";
+        private readonly SearchService _searchService;
+
+        public BarLocationController()
         {
-            return new string[] { "value1", "value2" };
+            _searchService = new SearchService(new QueryBuilder("AIzaSyBRlldhAClDuieSD7DBdiqky4N-lkhmb6A"));
+        }
+
+        [HttpGet("bars/nearby")]
+        public string GetNearbyBars(float latitude, float longitude, int radius)
+        {
+            var location = new Coordinates(latitude, longitude);
+            return SearchNearbyAsync(radius, location, BarType).Result;
+        }
+
+        private Task<string> SearchNearbyAsync(int radius, Coordinates location, string type)
+        {
+            return _searchService.NearbySearchAsync(location, radius, type).Result.Content.ReadAsStringAsync();
         }
     }
 }
