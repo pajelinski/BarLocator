4316793c57f81ee4e24d4db5f57455b79eb9e56d
Pawel Jelinski <Pawel.Jelinski@powel.com>
2016-10-11 11:33:27 +0200

    Added SearchService + tests

---
+++ BarLocatorService/src/BarLocatorService/Services/SearchService.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BarLocatorService.Domain;
+
+namespace BarLocatorService.Services
+{
+    public class SearchService
+    {
+        private readonly QueryBuilder _queryBuilder;
+        private HttpClient _httpClient;
+
+        public SearchService(QueryBuilder queryBuilder)
+        {
+            _queryBuilder = queryBuilder;
+            _httpClient = new HttpClient();
+        }
+
+        public Task<HttpResponseMessage> NearbySearchAsync(Coordinates location, int radius, string type)
+        {
+            return _httpClient.GetAsync(_queryBuilder.BuildQuery(location, radius, type));
+        }
+    }
+}
---
+++ BarLocatorService/test/BarLocatorService.Test/SearchServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using BarLocatorService.Domain;
+using BarLocatorService.Services;
+using Xunit;
+
+namespace BarLocatorService.Test
+{
+    public class SearchServiceTest
+    {
+        [Fact]
+        public void GivenApiKeyLocationRadiusAndType_NearbySearchShouldCreateWebRequestAndReturnResponseWithOkStatusCode() 
+        {
+            const string apiKey = "AIzaSyBRlldhAClDuieSD7DBdiqky4N-lkhmb6A";
+
+            var coordinates = new Coordinates(-33.8670522f, 151.1957362f);
+            var searchService = new SearchService(new QueryBuilder(apiKey));
+            var result = searchService.NearbySearchAsync(coordinates, 5000, "restaurant").Result;
+            var httpContent = result.Content.ReadAsStringAsync().Result;
+
+            Assert.NotNull(result);
+            Assert.NotNull(httpContent);
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+        }
+    }
+}
