2a9b712cc224cad8270c7c6050bbb8487f431964
Pawel Jelinski <Pawel.Jelinski@powel.com>
2016-10-12 14:53:23 +0200

    Added BarLocatorClient

---
+++ BarLocatorClient/BarLocatorClient/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BarLocatorClient.Startup))]
+namespace BarLocatorClient
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
