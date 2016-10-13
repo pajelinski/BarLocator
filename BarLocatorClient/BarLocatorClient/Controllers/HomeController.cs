ac924e3d1cfe0e62ddd7cc669f3f9a42f20abbb9
Pawel Jelinski <Pawel.Jelinski@powel.com>
2016-10-13 13:45:20 +0200

    Simple client implentation

--- BarLocatorClient/BarLocatorClient/Controllers/HomeController.cs
+++ BarLocatorClient/BarLocatorClient/Controllers/HomeController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Net.Http;
 using System.Web.Mvc;
 
 namespace BarLocatorClient.Controllers
@@ -11,6 +9,18 @@ namespace BarLocatorClient.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetNearbyBarLocations()
+        {
+            var httpClient = new HttpClient {BaseAddress = new Uri("http://localhost:5000/")};
+            var result = 
+                httpClient
+                .GetStringAsync("/api/bars/nearby?latitude=50.0618971&longitude=19.9345673&radius=2000")
+                .Result;
+
+            return Json(result, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult About()
